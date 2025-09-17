a6d49fb5b624a66f2400dc1ad45436d1261fb0d7
fk1352 <fk1352@g.rit.edu>
2025-09-17 23:49:16 +0200

    My finished work

--- characters.py
+++ characters.py
@@ -1,7 +1,34 @@
 '''
-@ASSESSME.USERID: userID
+@ASSESSME.USERID: Faris Karić , fk1352
 @ASSESSME.AUTHOR: author, list of authors
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+def add_chars(char1, char2):
+    total = ord(char1) + ord(char2)
+    print("Sum of ASCII values: ",total)
+    print("Associated ASCII character: ",chr(total % 128))
 
+def subtract_chars(char1, char2):
+    difference = ord(char1) - ord(char2)
+    print("Difference of ASCII values: ",difference)
+    if 0 <= difference <= 1000:
+        print("Associated ASCII character:", chr(difference))
+    else:
+        print("No valid ASCII charactzer for this difference")
+
+def main():
+    first = input("Enter first character: ")
+    second = input("Enter second character: ")
+    add_chars(first, second)
+    subtract_chars(first, second)
+
+if __name__ == "__main__":
+    main()
+
+#COMMENT1: when i type the first and second character for ex. q and M, I don't see any unusual things other than 
+#           associated characters which are > and $ .
+#COMMENT2: when I type more than two characters, some unusual errors occur stating that the def main is incorrect or
+#           something like that. 
+# When I did the assignment I didnt have access to internet for some reason so I couldn't research the problem on google, sorry...
+#           Will check the problem at school during the week...
--- scope.py
+++ scope.py
@@ -4,3 +4,31 @@
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL = "Hello"
+INT_GLOBAL = 42
+FLOAT_GLOBAL = 3.14
+
+def print_param(parameter):
+    print("Parameter value: ",parameter)
+
+def print_local():
+    local_variable = "I am local"
+    print("Local variable value: ",local_variable)
+
+def print_which():
+    STRING_GLOBAL = "Shadowed!"
+    print("Local STRING_GLOBAL inside print_which():", STRING_GLOBAL)
+
+
+def main():
+    print_param(STRING_GLOBAL)
+    print_param(INT_GLOBAL)
+    print_param(FLOAT_GLOBAL)
+    local_variable = "Different local value"
+    print_local()
+    print_which()
+    print("Global STRING_GLOBAL after print_which():", STRING_GLOBAL)
+
+if __name__ == "__main__":
+    main()
